b17d354a13050d553841971e1b90d0fb86c933df
Guillaume Turri <g.turri@criteo.com>
2024-07-10 20:25:58 +0200

    more robust url replacement
    because xcancel returns a 403 on url with a query string

--- toxcancel.js
+++ toxcancel.js
@@ -1,6 +1,6 @@
 var location = window.location;
 var currentLoc = location.href;
-var newLoc = currentLoc.replace("mobile.x.com", "xcancel.com")
-	.replace("x.com", "xcancel.com");
+var newLoc = "https://xcancel.com" + // don't just replace x.com with xcancel.com since mobile.xcancel.com does not exist
+	location.pathname; // skip a potential query string since it is not supported by xcancel
 console.log("redirect from " + currentLoc + " to " + newLoc);
 location.replace(newLoc);
